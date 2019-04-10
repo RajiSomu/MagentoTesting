27db666269383326e299a99c286687ef1a09cc0f
Priyanka <Priyanka@DESKTOP-5EL6MDO>
2019-04-10 19:25:58 +0530

    first commit

---
+++ MagentoTesting/src/main/java/Login.java
@@ -0,0 +1,34 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login {
+	WebDriver driver;
+
+	public Login(WebDriver driver) {
+		
+		this.driver = driver;
+		
+	}
+     By email= By.id("email");
+     By pwd=By.id("pass");
+     By login = By.id("send2");
+	public void typeEmail() 
+	{
+		driver.findElement(email).sendKeys("priyankavijaykumar555@gmail.com");		
+	}
+	public void typepassword() 
+	{
+		driver.findElement(pwd).sendKeys("Welcome123");
+		
+	}
+	public void clickOnLogin()
+	{
+		
+		driver.findElement(login).click();
+	}
+
+	
+
+}
