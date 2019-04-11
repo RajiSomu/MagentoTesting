f43fab6c901d85a2ea6bdea1522f68c83f7d875f
Priyanka <Priyanka@DESKTOP-5EL6MDO>
2019-04-11 18:02:45 +0530

    third commit

--- MagentoTesting/src/test/java/MagentoTest.java
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -3,7 +3,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest {
@@ -11,8 +11,8 @@ public class MagentoTest {
 	public void validLogin()
 	
 	{
-		WebDriver driver=new FirefoxDriver();
-		driver.manage().window().maximize();
+		WebDriver driver=new ChromeDriver();
+		//driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://magento.com");
 		Welcome w=new Welcome(driver);
